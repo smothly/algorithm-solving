d54f3113a1ad7bed0d61fdca7066fa00bfef5a52
seungho <seungho546@naver.com>
2020-06-03 01:03:04 +0900

    Add boj-1173 problem

---
+++ boj-1173.py
@@ -0,0 +1,28 @@
+'''
+백준 1173번 운동
+링크: https://www.acmicpc.net/problem/1173
+풀이방법
+- 운동할 수 없는 경우만 잘 파악하고 최대 맥박 관리만 잘 해주면 된다.
+'''
+import sys
+# sys.stdin = open("input.txt", "r")
+
+N, m, M, T, R=map(int, input().split())
+cnt = 0
+X = m
+# 운동 할 수 없는 경우
+if X + T > M:
+    cnt = -1
+else:    
+    while N:
+        cnt += 1
+        # 운동
+        if X + T <= M:  
+            X += T
+            N -= 1
+        # 휴식
+        else:      
+            X -= R
+            if X < m:
+                X = m
+print(cnt)
--- boj-16235.py
+++ boj-16235.py
@@ -2,20 +2,48 @@
 백준 16235번 나무제테크
 링크: https://www.acmicpc.net/problem/16235
 풀이방법
-- 
--
+- 빡구현 문제
+- 핵심은 여러개의 나무를 한좌표안에 갖고 있는 것이다...
 '''
 
 from collections import defaultdict
 
-def spring():
-    # 나이만큼 양분 먹고 나이가 1 증가
-    # 여러 개 나무 있으면 
+direction = [[-1,0],[0,-1],[1,0],[0,1],[1,1],[1,-1],[-1,1],[-1,-1]]
+def year():
+    # 봄 & 여름
+    for (r, c), tree in trees.items():
+        tree.sort()
+        temp_tree = []
+        sumv = 0
+        for t in tree:
+            # 나이만큼 양분먹기
+            if t <= board[r][c]:
+                board[r][c] -= t
+                temp_tree.append(t + 1)
+            # 양분 부족 -> 여름에 나이를 2로나눈 값이 양분으로 추가
+            else:
+                sumv += t//2
+
+        trees[(r, c)] = temp_tree
+        board[r][c] += sumv
+
+    # 가을
+    new_tree = []
+    for (r, c), tree in trees.items():
+        for t in tree:
+            if t%5 == 0:
+                for d in direction:
+                    move_r = r + d[0]
+                    move_c = c + d[1]
+                    if 0 <= move_r < N and 0 <= move_c < N:
+                        new_tree.append((move_r, move_c))
+    for r, c in new_tree:
+        trees[(r, c)].append(1)
+    
+    # 겨울
     for r in range(N):
         for c in range(N):
-            if MAP[r][c] != 0:
-                return
-
+            board[r][c] += A[r][c]
 
 
 import sys
@@ -27,21 +55,19 @@ A = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
 
 # 처음에 양분 맵
-MAP = [[5] * N for _ in range(N)]
+board = [[5] * N for _ in range(N)]
 
 # 여러개가 있을 수 있어서 리스트 딕트
 trees = defaultdict(lambda: [])
-tree_count = 0
 
+# 나무 입력
 for _ in range(M):
     x, y, z = map(int, sys.stdin.readline().split())
-    tress[(x-1, y-1)].append(z)
-    tree_count += 1
+    trees[(x-1, y-1)].append(z)
 
+# 제테크 진행
 for k in range(K):
-    spring()
-    summer()
-    fall()
-    winter()
+    year()
 
-print(A, trees)
+# 나무 개수 출력
+print(sum([len(tree) for tree in trees.values()]))
